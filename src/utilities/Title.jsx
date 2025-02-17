29e211ce1f55b1320a3cadad153a9ed32a060a17
rakib-hasan-78 <rakibwebdev78@gmail.com>
2025-02-18 01:05:07 +0600

    title component created...

---
+++ src/utilities/Title.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Title = ({tag, className, text}) => {
+    const TAG = `h${tag}`;
+    return (
+        <TAG className={` ${className}`}>
+            {text}
+        </TAG>
+    );
+};
+
+Title.propTypes = {
+    tag:PropTypes.string.isRequired,
+    className: PropTypes.string,
+    text: PropTypes.string.isRequired, 
+}
+export default Title;
