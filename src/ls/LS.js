98cf78dd5d7daeedbdb535eb7e180518493a5875
rakib-hasan-78 <rakibwebdev78@gmail.com>
2025-02-20 03:29:12 +0600

    local storage handlers completed...

---
+++ src/ls/LS.js
@@ -0,0 +1,25 @@
+
+// get local storage ===>
+const getLS = () => {
+
+    const LS = localStorage.getItem('blog');
+    return LS ? JSON.parse(LS) : [];
+}
+
+// save data to LS ====>>
+
+const saveLS = (blog) =>{
+    const saveItem = JSON.stringify(blog);
+    localStorage.setItem('blog', saveItem);
+}
+
+// addToLS function to pick data IDs ===>
+    
+const addToLS = id=>{
+    const addID =  getLS();
+    addID.push(id);
+    saveLS(addID); 
+}
+
+export { getLS, addToLS};
+
